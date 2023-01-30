7a8904cb637f8dc7f72635d75a60d9d53c5cf520
admin_user <rinaafanaseva@mail.ru>
2023-01-30 15:58:53 +0300

    1

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import requests
+import sys
+import os
+
+COORDS = 150, -30
+SPN = '1,1'
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS[0]},{COORDS[1]}&spn={SPN}&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
