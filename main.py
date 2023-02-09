f2280cd5ed4ebeeb221431e7736ff757e38909a2
admin_user <rinaafanaseva@mail.ru>
2023-02-09 21:50:41 +0300

    2

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import requests
 import sys
 import os
 
-COORDS = 150, -30
-SPN = '1,1'
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS[0]},{COORDS[1]}&spn={SPN}&l=map"
+COORDS = 37.677751, 55.757718
+SPN = '0.016457,0.00619'
+scale = 1.0
+z = 10
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS[0]},{COORDS[1]}&spn={SPN}&l=map&z={z}&scale={scale}"
 response = requests.get(map_request)
 
 if not response:
@@ -14,20 +16,44 @@ if not response:
     print("Http статус:", response.status_code, "(", response.reason, ")")
     sys.exit(1)
 
-# Запишем полученное изображение в файл.
 map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
 
-# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+clock = pygame.time.Clock()
+waiting = True
+while waiting:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            os.remove(map_file)
+            sys.exit()
+    clock.tick(30)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_PAGEUP]:
+        if scale < 4:
+            scale = float(int(scale) + 1)
+            print(scale)
+    if keys[pygame.K_PAGEDOWN]:
+        if scale > 1:
+            scale = float(int(scale) - 1)
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS[0]},{COORDS[1]}&spn={SPN}&l=map&z={z}&scale={scale}"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
